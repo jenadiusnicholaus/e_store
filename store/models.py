9f083fb6ac4eb4e20d2ac9d8a139e0047f24924d
unknown <George1215>
2020-11-16 17:28:19 +0300

    checkout finishup

--- store/models.py
+++ store/models.py
@@ -64,6 +64,8 @@ class OrderItem(models.Model):
     quantity = models.IntegerField(default=0, null=True, blank=True)
     date_added = models.DateTimeField(auto_now_add=True)
 
+    # Using property decorator to get the price of the product multiplied by the quantity
+
     @property
     def get_total(self):
         total = self.product.price * self.quantity
--- store/views.py
+++ store/views.py
@@ -18,14 +18,22 @@ def cart(request):
         items = order.orderitem_set.all()
     else:
         items = []  # when a user isn't authenticated
-        order = {'get_cart-total':0, 'get_cart_items':0}
+        order = {'get_cart-total': 0, 'get_cart_items': 0}
 
-    context = {'items': items, 'order':order}
+    context = {'items': items, 'order': order}
     return render(request, 'store/cart.html', context)
 
 
 def checkout(request):
-    context = {}
+    if request.user.is_authenticated:
+        customer = request.user.customer
+        order, created = Order.objects.get_or_create(customer=customer, complete=False)
+        items = order.orderitem_set.all()
+    else:
+        items = []  # when a user isn't authenticated
+        order = {'get_cart-total': 0, 'get_cart_items': 0}
+
+    context = {'items': items, 'order': order}
     return render(request, 'store/checkout.html', context)
 
 
